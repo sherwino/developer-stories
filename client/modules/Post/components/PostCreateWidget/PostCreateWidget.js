8f601ef29a22ee1bea79ecda30ddf06bc42b04f7
sherwino <sherwination@gmail.com>
2017-11-19 13:50:42 -0500

    added an event date field, and sev changes

--- client/modules/Post/components/PostCreateWidget/PostCreateWidget.js
+++ client/modules/Post/components/PostCreateWidget/PostCreateWidget.js
@@ -21,8 +21,11 @@ export class PostCreateWidget extends Component {
       <div className={cls}>
         <div className={styles['form-content']}>
           <h2 className={styles['form-title']}><FormattedMessage id="createNewPost" /></h2>
-          <input placeholder={this.props.intl.messages.authorName} className={styles['form-field']} ref="name" />
-          <input placeholder={this.props.intl.messages.postTitle} className={styles['form-field']} ref="title" />
+          <input placeholder={this.props.intl.messages.postTitle} className={styles['form-field']} ref="title" /> {/* event title */}
+          <input placeholder={this.props.intl.messages.authorName} className={styles['form-field']} ref="name" /> {/* event type dropdown */}
+          <input placeholder={this.props.intl.messages.dateOfEvent} className={styles['form-field']}
+          id="event-date" type="month" ref="event-month" min="1950-01" required pattern="[0-9]{4}-[0-9]{2}"/> {/* event date, works diff on firefox and chrome */}
+          <span class="validity"></span>
           <textarea placeholder={this.props.intl.messages.postContent} className={styles['form-field']} ref="content" />
           <a className={styles['post-submit-button']} href="#" onClick={this.addPost}><FormattedMessage id="submit" /></a>
         </div>
