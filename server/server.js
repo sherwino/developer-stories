bf2c3788c3d2ebf4ef3fc13911a24fdd74701855
sherwino <sherwination@gmail.com>
2017-11-19 22:42:29 -0500

    fav-icon update

--- server/server.js
+++ server/server.js
@@ -77,7 +77,7 @@ const renderFullPage = (html, initialState) => {
 
         ${process.env.NODE_ENV === 'production' ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
         <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'/>
-        <link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />
+        <link rel="shortcut icon" href="https://i.imgur.com/yCG1tMN.png" type="image/png" />
       </head>
       <body>
         <div id="root">${html}</div>
